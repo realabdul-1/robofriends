72949620965973042dc21e1a0744c2e4e5604cae
realabdul-1 <aljaadi.am@gmail.com>
2022-09-08 19:46:04 +0800

    Updates

---
+++ precache-manifest.79c2b1ed495e44ad228f9a5d1584b7a1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e00f2a8603c0be3123af",
+    "url": "/robofriends/static/css/main.d7fa7138.chunk.css"
+  },
+  {
+    "revision": "e00f2a8603c0be3123af",
+    "url": "/robofriends/static/js/main.e00f2a86.chunk.js"
+  },
+  {
+    "revision": "206e060bd7d3afd2158d",
+    "url": "/robofriends/static/js/1.206e060b.chunk.js"
+  },
+  {
+    "revision": "f773a7c4183281748778",
+    "url": "/robofriends/static/css/2.da3c75f0.chunk.css"
+  },
+  {
+    "revision": "f773a7c4183281748778",
+    "url": "/robofriends/static/js/2.f773a7c4.chunk.js"
+  },
+  {
+    "revision": "5a6fc926011dbcb5a003",
+    "url": "/robofriends/static/js/runtime~main.5a6fc926.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "63d23fc757de068f1ff7fa5f8bbf1bd4",
+    "url": "/robofriends/index.html"
+  }
+];
